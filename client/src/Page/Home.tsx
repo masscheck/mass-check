e48379a5a4144021811e223e4f909da9d1b11248
LeJon <lejonpun@gmail.com>
2021-08-05 22:53:45 +0800

    Updated Homepage & Loading animation
    - done homepage css
    - edited loading animation

--- client/src/Page/Home.tsx
+++ client/src/Page/Home.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { useLoadingSpinner } from '../Context/LoadingSpinnerContext';
+import Tweet from '../Component/Tweet';
+
+import './Home.scss';
 
 const Home: React.FC = () => {
+  
   const { setIsLoading } = useLoadingSpinner();
 
   const isLoading = () => {
@@ -16,8 +20,63 @@ const Home: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>HOME PAGE</h1>
+    <div className='home'>
+      <div className='home__header'>
+        <button className='home__header__verified'>
+          Verified Posts
+        </button>
+        <button className='home__header__unverified'>
+          Unverified Posts
+        </button>
+
+      </div>
+      <div className='home__view-area'>
+       <div className='home__view-area__tweet'>
+        <Tweet
+          name='Jill Chenraya'
+          tag='@jillcry'
+          content='#Malaysia recorded a total of 2,875 new #Covid19 cases on Thursday. This is the eighth consecutive day with the number of cases above 2,000. Read more at https://bit.ly/3neKgcD'
+          submitBy='Jackie Chan'
+          submitTime={new Date()}
+          authenticityScore={67}
+          stage='Verifying'
+        />
+       </div>
+       <div className='home__view-area__tweet'>
+        <Tweet
+          name='Jill Chenraya'
+          tag='@jillcry'
+          content='#Malaysia recorded a total of 2,875 new #Covid19 cases on Thursday. This is the eighth consecutive day with the number of cases above 2,000. Read more at https://bit.ly/3neKgcD'
+          submitBy='Jackie Chan'
+          submitTime={new Date()}
+          authenticityScore={67}
+          stage='Verifying'
+        />
+       </div>
+       <div className='home__view-area__tweet'>
+        <Tweet
+          name='Jill Chenraya'
+          tag='@jillcry'
+          content='#Malaysia recorded a total of 2,875 new #Covid19 cases on Thursday. This is the eighth consecutive day with the number of cases above 2,000. Read more at https://bit.ly/3neKgcD'
+          submitBy='Jackie Chan'
+          submitTime={new Date()}
+          authenticityScore={67}
+          stage='Verifying'
+        />
+       </div>
+       <div className='home__view-area__tweet'>
+        <Tweet
+          name='Jill Chenraya'
+          tag='@jillcry'
+          content='#Malaysia recorded a total of 2,875 new #Covid19 cases on Thursday. This is the eighth consecutive day with the number of cases above 2,000. Read more at https://bit.ly/3neKgcD'
+          submitBy='Jackie Chan'
+          submitTime={new Date()}
+          authenticityScore={67}
+          stage='Verifying'
+        />
+       </div>
+      </div>
+      
       <button onClick={() => isLoading()}>Test Loading Button</button>
     </div>
   );
