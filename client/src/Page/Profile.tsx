1e549683355f84327a38e5c9792534bee830bc31
Kev1069 <kevinchowkv@gmail.com>
2021-09-17 19:51:52 +0800

    Linked User Credibility Score API to Profile Frontend

--- client/src/Page/Profile.tsx
+++ client/src/Page/Profile.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Profile.scss';
 import Activity from '../Component/Activity';
 
+import { useNotification } from '../Context/NotificationContext';
+import { LocalStorageEnum } from '../Util/Constant/LocalStorageEnum';
+import { getUserCredibilityScore } from '../Util/API/UserProfileAPI';
+
 const Profile: React.FC = () => {
+  const { errorToastPersistent } = useNotification();
+  const uid = localStorage.getItem(LocalStorageEnum.UID);
+  const [CredibilityScore, setCredibilityScore] = useState(0);
+
+  useEffect(() => {
+    async function loadUserCredibilityScore(uid) {
+      const userCredibilityScore = await getUserCredibilityScore(uid);
+      setCredibilityScore(userCredibilityScore.data['credibilityScore']);
+    }
+    loadUserCredibilityScore(uid);
+  }, [uid]);
+
   return (
     <div className='profile'>
       <div className='profile__top'>
@@ -12,20 +28,26 @@ const Profile: React.FC = () => {
             <div className='profile__top__wallet__content__left'>
               <div>XPX Balance </div>
               <img src={require(`../Asset/proximax.png`).default} />
-              <br/>
-              <div className='profile__top__wallet__content__left__amount'>100</div>
+              <br />
+              <div className='profile__top__wallet__content__left__amount'>
+                100
+              </div>
             </div>
             <div className='profile__top__wallet__content__right'>
-              <button className='profile__top__wallet__content__right__button'>View in Wallet</button>
+              <button className='profile__top__wallet__content__right__button'>
+                View in Wallet
+              </button>
             </div>
           </div>
         </div>
         <div className='profile__top__credibility'>
           <div className='profile__top__credibility__title'>My Credibility</div>
           <div className='profile__top__credibility__score'>
-            <div >Credibility Score</div>
-              <br/>
-            <div className='profile__top__credibility__score__value'>25/100</div>
+            <div>Credibility Score</div>
+            <br />
+            <div className='profile__top__credibility__score__value'>
+              {CredibilityScore}/100
+            </div>
           </div>
         </div>
       </div>
