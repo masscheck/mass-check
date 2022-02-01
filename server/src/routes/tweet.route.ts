28f4421d486ecac23c8965e0e4fe58684a277eb1
Yong Ming-Jia <yongmingjia0112@gmail.com>
2022-02-01 11:26:14 +0800

    update AI score

--- server/src/routes/tweet.route.ts
+++ server/src/routes/tweet.route.ts
@@ -21,6 +21,19 @@ router.post('/create-tweet', async (req, res, next) => {
 
   try {
     aiScore = await getAIScore(tweetContent);
+
+    if (aiScore >= 0.95) {
+      const penalty = Math.floor(Math.random() * 15) / 100;
+      aiScore -= penalty;
+    } else if (aiScore < 0.2) {
+      if (aiScore < 0.05) {
+        aiScore += 0.1;
+      }
+
+      const penalty = Math.floor(Math.random() * 20) / 100;
+      aiScore += penalty;
+    }
+
     logger.verbose('AI Server - Retrieve Score', aiScore);
   } catch (err) {
     logger.verbose('AI Server - Failed from to get AI Score');
